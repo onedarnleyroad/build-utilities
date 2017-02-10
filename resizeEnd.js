189114c9a825c3d874901a75fb0fa9bad8160ed2
Nathan Edwards <nathan@onedarnleyroad.com>
2017-02-10 16:46:07 +0000

    remove old code

--- resizeEnd.js
+++ resizeEnd.js
@@ -4,19 +4,8 @@
         clearTimeout(resizeTimer);
         resizeTimer = setTimeout(function() {
 
-            // Recalculate:
-            // search extras top margin
-            placeSearchExtras();
-            calcStickyElements();
-
-            // Rerun:
-            // Flickity has its own internal handler so no need to put it here
-            // CustomSelecct
-            $('[data-custom-select], .customSelect').trigger('render');
-
             // raise resize end event.
             $(window).trigger('resizeEnd');
-
 
         }, 250);
     });
