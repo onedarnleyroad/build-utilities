33b76a279d29d367386bf1773465dd76c327f0c8
Nathan Edwards <nathan@onedarnleyroad.com>
2017-02-13 11:13:38 +0000

    use index as a helper for loading scripts

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 
+// this is just a helper, a store
+// for the node module locations
+// so that each time we start adding
+// to a project, we don't have to remember
+// the node path for an installed file,
+// and the result is a leaner, much faster
+// configurable
+
+var _1dr = function( keyword ) {
+    return './node_modules/build-utilities/'+keyword+'.js';
+}
+
+// @TODO = some kind of function that outputs instructions so we can remember what
+// the hell each of these does.
+// and obviously to increase this further
+
+module.exports = {
+
+    locations: {
+
+        // ours
+            'resizeEnd': _1dr('resizeEnd'),
+            'oneModals': _1dr('oneModals'),
+            'classToggler': _1dr('classToggler'),
+
+
+        // vendor
+            'jquery' : './node_modules/jquery/dist/jquery.min.js',
+            'scrollMonitor' : './node_modules/scrollmonitor/scrollMonitor.js',
+
+    },
+
+    load: function( ids, add ) {
+
+        var fileList = [];
+
+        ids.forEach( ( id ) => {
+
+            if ( this.locations.hasOwnProperty( id ) ) {
+                fileList.push( this.locations[id] )
+            } else {
+                console.log( "Couldn't Find " + id + " in the file list store!" );
+            }
+        }
+
+        return fileList.concat( add );
+
+    }
+
+};
